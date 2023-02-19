8a36b43dad944c720a005de95436accd6cb91ef2
rajatnai49 <rajatnai49@gmail.com>
2023-02-19 14:58:38 +0530

    changes made by rajat

--- models/appointment.js
+++ models/appointment.js
@@ -9,27 +9,10 @@ const AppointmentSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Doctor',
     },
-    PrescriptionId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Prescription',
-        default: null,
-    },
     AppointmentDate: {
         type: String,
         default: 'none',
-    },
-    Disease: {
-        type: String,
-        default: 'none',
-    },
-    Status: {
-        type: String,
-        default: 'none',
-    },
-    SerialNumber: {
-        type: Number,
-        default: null,
-    },
+    }
 });
 
 module.exports = mongoose.model('Appointment', AppointmentSchema);
--- routes/patient.js
+++ routes/patient.js
@@ -26,69 +26,7 @@ router.get('/patientlogin', (req, res) => {
 });
 
 router.get('/patienthome', (req, res) => {
-    const futureappointment = [];
-    const pastappointment = [];
-    const data = {};
-    const obj1 = {};
-    const SortPatientPromise = new Promise((resolve, reject) => {
-        Appointment.find({ PatientId: req.user._id })
-            .populate('PrescriptionId')
-            .exec((err, appointments) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(appointments);
-                }
-            });
-    });
-    SortPatientPromise
-        .then((result) => {
-            result.forEach((item) => {
-                const obj = {}
-                const data1 = new Date(item.AppointmentDate)
-                const data2 = new Date()
-                obj.PatientId = item.PatientId;
-                obj.DoctorId = item.DoctorId;
-                obj.PrescriptionId = item.PrescriptionId;
-                obj.AppointmentDate = convert(item.AppointmentDate);
-                obj.AppointmentTime = ConvertChosenTime(item.AppointmentDate);
-                obj.Disease = item.Disease;
-                obj.Status = item.Status;
-                obj.SerialNumber = item.SerialNumber;
-                if (item.PrescriptionId == null) {
-                    obj.Symptoms = null;
-                    obj.Tests = null;
-                    obj.Medicines = null;
-                    obj.Remarks = null;
-                    obj.DoctorSignature = null;
-                } else {
-                    obj.Symptoms = item.PrescriptionId.Symptoms;
-                    obj.Tests = item.PrescriptionId.Tests;
-                    obj.Medicines = item.PrescriptionId.Medicines;
-                    obj.Remarks = item.PrescriptionId.Remarks;
-                    obj.DoctorSignature = item.PrescriptionId.DoctorSignature;
-                }
-                // if (date1.getTime() > date2.getTime()) {
-                futureappointment.push(obj);
-                // } else {
-                pastappointment.push(obj);
-                // }
-            });
-        }).catch((error) => {
-            console.log(`Error From promise 1 a) ${error}`);
-        });
-    Promise.all([SortPatientPromise])
-        .then((result) => {
-            data.futureappointment = futureappointment;
-            data.pastappointment = pastappointment;
-            data.user = req.user;
-            console.log(result);
-            return res.status(200).json(data);
-            // res.render('patienthome', { data });
-        })
-        .catch((error) => {
-            console.log(`Error From promise 1 c) ${error}`);
-        });
+
 
 })
 // +Add isAuthorizedPatient
