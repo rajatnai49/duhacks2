75e9d73518bda309d21ac4c40d40a12c6691229c
rajatnai49 <rajatnai49@gmail.com>
2023-02-19 12:19:30 +0530

    changes in patient-homhome

--- routes/docter.js
+++ routes/docter.js
@@ -7,12 +7,6 @@ const { isLoggedIn } = require('./helper')
 
 const router = express.Router()
 
-// GET
-// router.get('/doctorregistration', (req, res) => {
-//     const data = {};
-//     data.user = req.user;
-//     res.render('doctorregistration', { data });
-// })
 router.get('/doctorlogin', (req, res) => {
     const data = {}
     data.user = req.user
@@ -31,32 +25,6 @@ router.post(
         failureRedirect: '/doctorlogin',
     })
 )
-// router.post('/doctorregistration', (req, res) => {
-//     const temp = new Doctor({
-//         username: req.body.username,
-//         usertype: req.body.usertype,
-//         name: req.body.name,
-//         phone: req.body.phone,
-//         specialization: req.body.specialization,
-//         address: req.body.address,
-//     });
-
-//     Doctor.register(temp, req.body.password, (err, item) => {
-//         if (err) {
-//             console.log(err);
-//             const data = {};
-//             data.user = req.user;
-//             console.log(item);
-//             res.render('doctorregistration', { data });
-//         }
-//         passport.authenticate('doctorlocal')(req, res, () => {
-//             res.send("created")
-//             // res.redirect('/doctorhome');
-//         });
-//     });
-// });
-
-// PUT
 
 router.put('/doctorhome/:id/edit', (req, res) => {
     Doctor.findByIdAndUpdate(req.params.id, req.body, req.body, (err, item) => {
